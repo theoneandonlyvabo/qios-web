66b1b19386a0f391bc2274248ceeb2fd876bca98
Airel Adrivano <165012306+theoneandonlyvabo@users.noreply.github.com>
2026-05-16 08:07:13 +0700

    Frontend Adjustments

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   async headers() {
@@ -23,4 +24,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+})(nextConfig);
